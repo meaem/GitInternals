dcfd59ee589b4f5059d7b5f67e4e77d931914226
Ahmed Mohamed Abdelaal <aabdelaal@gmail.com>
2022-04-19 01:41:30 +0200

    stage 3 finished

--- Git Internals/task/src/gitinternals/Main.kt
+++ Git Internals/task/src/gitinternals/Main.kt
@@ -1,11 +1,116 @@
 package gitinternals
 
 import java.io.ByteArrayOutputStream
+import java.io.File
 import java.io.FileInputStream
+import java.time.LocalDateTime
+import java.time.ZoneOffset
+import java.time.format.DateTimeFormatter
 import java.util.zip.InflaterOutputStream
 
+open class GitObject() {
+    companion object {
+        val linePattern = "^(\\w+) (.+)$".toRegex()
+        val authorPattern = "^(\\w+) (\\w+) <(.+)> (\\d+) ([+-])(\\d{2})(\\d{2})$".toRegex()
+        fun createObject(c: String): GitObject {
+            val lines = c.lines()
+            val groups = linePattern.findAll(lines.first())
+            if (groups.first().groupValues[1] == "blob") {
+                return GitBlobObject(lines.subList(1, lines.size).joinToString("\n"))
+            } else {//if (groups.first().groupValues[1] == "commit") {
+                return GitCommitObject().also {
+                    var nextLinesAreMessage = false
+                    it.message = ""
+                    it.parent = ""
+                    for (line in lines.withIndex()) {
+                        if (nextLinesAreMessage) {
+                            it.message += line.value + "\n"
+                        } else if (line.value.isBlank()) {
+                            nextLinesAreMessage = true
+                        } else if (line.index > 0) {
+                            val linegroups = linePattern.findAll(line.value).first()
+                            when (linegroups.groupValues[1]) {
+                                "tree" -> it.tree = linegroups.groupValues[2]
+                                "parent" -> {
+                                    it.parent += (if (it.parent.isEmpty()) "" else " | ") + linegroups.groupValues[2]
+                                }
+                                "author" -> {
+                                    it.author = parseContributor(line.value, "author")
+                                }
+                                "committer" -> {
+                                    it.commiter = parseContributor(line.value, "committer")
+                                }
 
+                            }
+                        }
+                    }
+                    it.message = it.message.trimEnd('\n')
+                }
+            }
+
+        }
+
+        private fun parseContributor(line: String, type: String): String {
+            val commitType = if (type == "committer") "commit" else "original"
+            val authGroups = authorPattern.findAll(line).first()
+            val name = authGroups.groupValues[2]
+            val email = authGroups.groupValues[3]
+            val datetime = authGroups.groupValues[4]
+            val zoneSign = authGroups.groupValues[5]
+            val zoneHours = authGroups.groupValues[6]
+            val zoneMinuts = authGroups.groupValues[7]
+            val zHours = zoneHours.toInt() * if (zoneSign == "-") -1 else 1
+            val zMinuts = zoneMinuts.toInt()
+
+            val instant = LocalDateTime.ofEpochSecond(
+                datetime.toLong(),
+                0,
+                ZoneOffset.ofHoursMinutes(zHours, zMinuts)
+            )
+//                                    val instant = Instant.ofEpochSecond(datetime.toLong())
+            val formtter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+            val datetimeString = formtter.format(instant)
+            return "$name $email $commitType timestamp: $datetimeString $zoneSign$zoneHours:$zoneMinuts"
+        }
+    }
+}
+
+class GitBlobObject(val content: String) : GitObject() {
+    override fun toString(): String {
+        return "*BLOB*\n$content"
+    }
+}
+
+class GitTreeObject(val content: String) : GitObject() {
+    override fun toString(): String {
+        return "*TREE*\n$content"
+    }
+}
+
+class GitCommitObject(
+
+) : GitObject() {
+    lateinit var tree: String
+    var parent: String = ""
+    lateinit var author: String
+    lateinit var commiter: String
+    lateinit var message: String
+
+    override fun toString(): String {
+        return "*COMMIT*\n" +
+                "tree: $tree\n" +
+                (if (parent.isNotBlank()) "parents: $parent\n" else "") +
+                "author: $author\n" +
+                "committer: $commiter\n" +
+                "commit message:\n$message"
+    }
+}
+
+val debugFile = File("debug.txt")
 fun main() {
+//    "+0300".substring(1).split("\\d{2}".toRegex()).map { it.toInt() }
+//    val linePattern = "^(\\w+) (.+)$".toRegex()
+//    println(linePattern.findAll("commit 216").first().groupValues[1])
     println("Enter .git directory location:")
     val gitDirPath = readln()
 
@@ -23,10 +128,9 @@ fun main() {
     val result = bos1.toByteArray()
     val contentStr = String(result)
         .replace(Char(0), '\n')
-    contentStr.lines().first().let {
-        it.split(" ").let {
-            println("type:${it[0]} length:${it[1]}")
-        }
-    }
+    debugFile.writeText(contentStr)
+
+    val gitObj = GitObject.createObject(contentStr)
+    println(gitObj)
 
 }
