c26a7b3a722eb23a749122cfcd5738a3e9863865
Ahmed Mohamed Abdelaal <aabdelaal@gmail.com>
2022-04-18 19:31:30 +0200

    stage 2 finished

--- Git Internals/task/src/gitinternals/Main.kt
+++ Git Internals/task/src/gitinternals/Main.kt
@@ -1,19 +1,32 @@
 package gitinternals
 
-import java.io.File
-import java.util.zip.Inflater
+import java.io.ByteArrayOutputStream
+import java.io.FileInputStream
+import java.util.zip.InflaterOutputStream
+
 
 fun main() {
-    println("Enter git object location:")
-    val objPath = readln()
-    val objFile = File(objPath)
-    val content = objFile.readBytes()
-    val result = ByteArray(1024)
-    val decompressor = Inflater()
-    decompressor.setInput(content)
-    val actualNum = decompressor.inflate(result)
-    decompressor.end()
-    val contentStr = String(result,0,actualNum-1)
-        .replace(Char(0),'\n')
-    println(contentStr)
+    println("Enter .git directory location:")
+    val gitDirPath = readln()
+
+    println("Enter git object hash:")
+    val objHash = readln()
+    val objPath = "$gitDirPath/objects/${objHash.substring(0, 2)}/${objHash.substring(2)}"
+//    println(objPath)
+    val fis = FileInputStream(objPath)
+    val bos1 = ByteArrayOutputStream()
+    val ios = InflaterOutputStream(bos1)
+    var ch: Int
+    while (fis.read().also { ch = it } != -1) {
+        ios.write(ch)
+    }
+    val result = bos1.toByteArray()
+    val contentStr = String(result)
+        .replace(Char(0), '\n')
+    contentStr.lines().first().let {
+        it.split(" ").let {
+            println("type:${it[0]} length:${it[1]}")
+        }
+    }
+
 }
